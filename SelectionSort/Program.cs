8f680d6d5d6da59af027a6434c9cfbd070ca2918
Pranay Neema <neema.pranay23@gmail.com>
2020-03-02 16:09:08 +0530

    Adding implementation of Selection Sort
    This implementation assume first element of array as minimum value.

---
+++ SelectionSort/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        static void SelectionSort(ref int[] A)
+        {
+            for (int i = 0 ; i < A.Length - 1 ; i++)
+            {
+                int swapIndex = i;
+                int min = A[i];
+                for (int j = i ; j < A.Length - 1; j++)
+                {
+                    if (min > A[j])
+                    {
+                        min = A[j];
+                        swapIndex = j;
+                    }
+                }
+                int temp = A[i];
+                A[i] = A[swapIndex];
+                A[swapIndex] = temp;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the no of element");
+            int length = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the space seprated values");
+            string inputLine = Console.ReadLine();
+            string[] inputarray = inputLine.Split(' ');
+            int[] A = new int[length];
+            A = Array.ConvertAll(inputarray, int.Parse);
+            SelectionSort(ref A);
+            foreach( int val in A )
+                Console.WriteLine(val);
+            Console.Read();
+                
+        }
+    }
+}
